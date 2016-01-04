d66ae266cbe59ba2290a876d2ff2ac3eabef85ff
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-04 16:04:24 +0200

    Update essay

--- lib/activerecord-positionable/essay.rb
+++ lib/activerecord-positionable/essay.rb
@@ -3,12 +3,20 @@ module Essay
     def positionable?
       model_class.positionable_options.any?
     end
+
+    serialize do
+      { positionable: positionable? }
+    end
   end
 
   class AttributeRoles
     def position?
       list = model_class.attributes_marked_as_position
       !!list && list.include?(attribute_name)
+    end
+
+    serialize do
+      { is_position: position? }
     end
   end
 end
